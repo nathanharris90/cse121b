b54f29e1861d171070514b5b9c9681bbcd919cb6
nathan90@byui.edu <nathan90@byui.edu>
2023-09-23 21:48:11 -0600

    W02 Programming Exercises

---
+++ scripts/w02-task.js
@@ -0,0 +1,40 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = 'Nathan Harris';
+const currentYear = '2023';
+const profilePicture = 'images/me.jpeg';
+
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector('img');
+
+
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = currentYear;
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `Profile image of ${fullName}`);
+
+
+
+/* Step 5 - Array */
+const favoriteFoods = ['potatoes', 'corn', 'pizza'];
+foodElement.innerHTML = favoriteFoods;
+const newFood = 'cheese';
+favoriteFoods.push(newFood);
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+favoriteFoods.shift();
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+favoriteFoods.pop();
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+
+
+
+
